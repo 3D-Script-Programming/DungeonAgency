9240e30fc9b4661a5d7348aad7a405e6718fc0df
hwookim <gusdn9754@gmail.com>
2021-06-03 11:11:54 +0900

    feat: 캐릭터 컨트롤러 - GetHit, Die 구현
     - 피격 애니메이션을 위한 GetHit, Die 함수 구현
     - 미구현으로 스테이터스 호출 대신 고정값 부여

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    private Animator animator;
+
+    private int hp; 
+    private bool isDead;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        hp = 1000;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDead) return;
+    }
+
+    private void Die() {
+        isDead = true;
+    }
+
+    public void GetHit(int damage) {
+        animator.SetTrigger("GetHit");
+        hp -= damage;
+        animator.SetInteger("HP", hp);
+        if (hp < 1) Die();
+    }
+}
