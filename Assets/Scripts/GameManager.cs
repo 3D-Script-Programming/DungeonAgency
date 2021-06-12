e37cf78c892a06427a127f6b5b03c2896b578028
hwwokim <gusdn9754@gmail.com>
2021-06-12 19:02:11 +0900

    feat: 버튼 클릭을 통한 씬 이동 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -49,25 +49,29 @@ public class GameManager : MonoBehaviour
         player.GetRoom(2).PlaceMonster(0, player.GetMonster(8));
     }
 
-    public void moveMainScene()
+    public static void MoveMainScene()
     {
         SceneManager.LoadScene("MainScene");
     }
 
-    public void moveBattleScene()
+    public static void MoveBattleScene()
     {
         SceneManager.LoadScene("BattleScene");
-
     }
 
-    public void moveManageScene()
+    public static void MoveManageScene()
     {
         SceneManager.LoadScene("ManageScene");
     }
 
-    public void moveShopScene()
+    public static void MoveShopScene()
     {
         SceneManager.LoadScene("ShopScene");
+    }
+
+    public static void MoveSettingScene()
+    {
+        SceneManager.LoadScene("SettingScene");
     }
 
     public void SaveData() { }
---
+++ Assets/Scripts/UIEventManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIEventManager : MonoBehaviour
+{
+    public Button playButton;
+    public Button shopButton;
+    public Button settingButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButton.onClick.AddListener(GameManager.MoveManageScene);
+        shopButton.onClick.AddListener(GameManager.MoveShopScene);
+        settingButton.onClick.AddListener(GameManager.MoveSettingScene);
+    }
+}
