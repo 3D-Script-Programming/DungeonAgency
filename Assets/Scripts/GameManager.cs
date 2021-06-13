9da337170e74dbc97d0a13a57d205c0d9370aa72
hwwokim <gusdn9754@gmail.com>
2021-06-13 20:16:46 +0900

    feat: 임시 배치 코드 제거

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -34,20 +34,6 @@ public class GameManager : MonoBehaviour
         int[] rank = { 1, 1, 1, 1, 1, 1, 1, 1, 1 };
         player.AddRangeMonster(CharacterFactory.CreateMonsterList(rank));
 
-        // 임시 각 룸에 몬스터 배치
-        // 1번 룸
-        player.GetRoom(0).PlaceMonster(0, player.GetMonster(0));
-
-        // 2번 룸
-        player.GetRoom(1).PlaceMonster(0, player.GetMonster(3));
-        player.GetRoom(1).PlaceMonster(1, player.GetMonster(4));
-        player.GetRoom(1).PlaceMonster(2, player.GetMonster(5));
-
-
-        // 3번 룸 보스방 보스는 몬스터 리스트 0번으로 배치할 것!
-        player.GetRoom(2).Item = Item.CROWN;
-        player.GetRoom(2).PlaceMonster(0, player.GetMonster(8));
-
         audioSource = gameObject.GetComponent<AudioSource>();
     }
 
