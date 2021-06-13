4d8b6b1fbf8d3c9dc09cc615d192e843150ecc27
sadgsadg97 <sadgsadg97@gmail.com>
2021-06-13 14:34:47 +0900

    Character 수정 및 리스트 구현

---
+++ Assets/Scripts/ListController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private List<Character> monsters;
+    private List<GameObject> listItems;
+    private int money;
+    private RectTransform scrollViewContent;
+    void Start()
+    {
+        scrollViewContent = GameObject.Find("Content").GetComponent<RectTransform>();
+        scrollViewContent.sizeDelta = new Vector2(145.2759f, 30 * monsters.Count);
+        listItems = new List<GameObject>();
+        InstantiateItems();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetData(List<Character> monsters, int money)
+    {
+        this.monsters = monsters;
+        this.money = money;
+    }
+
+    public void InstantiateItems()
+    {
+        while (listItems.Count > 0)
+            Destroy(listItems[listItems.Count - 1]);
+
+        for (int i = 0; i < monsters.Count; i++) 
+        {
+            listItems.Add(Instantiate(Resources.Load<GameObject>("UI/listItem")));
+            listItems[i].GetComponent<listItemController>().SetText(monsters[i], money);
+            listItems[i].transform.SetParent(GameObject.Find("Content").transform);
+        }
+
+        scrollViewContent.sizeDelta = new Vector2(145.2759f, 60 * monsters.Count);
+    }
+}
---
+++ Assets/Scripts/listItemController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class listItemController : MonoBehaviour
+{
+    public TextMeshProUGUI name, level, str, bal, vtp, cp;
+    public GameObject btnHire;
+    public GameObject natureHolder;
+    public GameObject natureIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetText(Character character, int money)
+    {
+        if (character.GetNature() == Nature.FIRE)
+        {
+            natureIcon.GetComponent<Image>().sprite = Resources.Load("UI/fire", typeof(Sprite)) as Sprite;
+            natureHolder.GetComponent<Image>().sprite = Resources.Load("UI/slider_skill_frame_red", typeof(Sprite)) as Sprite;
+        }
+        else if (character.GetNature() == Nature.WATER)
+        {
+            natureIcon.GetComponent<Image>().sprite = Resources.Load("UI/water", typeof(Sprite)) as Sprite;
+            natureHolder.GetComponent<Image>().sprite = Resources.Load("UI/slider_skill_frame_blue", typeof(Sprite)) as Sprite;
+        }
+        else if (character.GetNature() == Nature.WIND)
+        {
+            natureIcon.GetComponent<Image>().sprite = Resources.Load("UI/wind", typeof(Sprite)) as Sprite;
+            natureHolder.GetComponent<Image>().sprite = Resources.Load("UI/slider_skill_frame_green", typeof(Sprite)) as Sprite;
+        }
+
+        name.text = character.GetName();
+        level.text = character.GetLevel().ToString();
+        str.text = character.GetSTR().ToString();
+        bal.text = character.GetBAL().ToString();
+        vtp.text = character.GetVTP().ToString();
+        cp.text = character.GetCP().ToString();
+
+        btnHire.GetComponentInChildren<TextMeshProUGUI>().text = character.GetPrice().ToString();
+
+        if (character.GetPrice() > money)
+            btnHire.GetComponent<Image>().sprite = Resources.Load("UI/btn_color_red", typeof(Sprite)) as Sprite;
+        else
+            btnHire.GetComponent<Image>().sprite = Resources.Load("UI/btn_color_green", typeof(Sprite)) as Sprite;
+    }
+
+    public void SetButtonActive(bool isActive)
+    {
+        if (isActive)
+        {
+            btnHire.SetActive(true);
+            btnHire.GetComponent<Image>().sprite = Resources.Load("UI/btn_color_green", typeof(Sprite)) as Sprite;
+        }
+        else
+        {
+            btnHire.SetActive(false);
+        }
+    }
+}
