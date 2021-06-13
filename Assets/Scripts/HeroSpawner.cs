e1f990a41afd9ed6a9be1d35b4df7e70c2ab85de
hwwokim <gusdn9754@gmail.com>
2021-06-13 13:21:56 +0900

    fix: 방 이동 중 용사 소환 시 오류 해결

--- Assets/Scripts/HeroSpawner.cs
+++ Assets/Scripts/HeroSpawner.cs
@@ -13,10 +13,11 @@ public class HeroSpawner : MonoBehaviour
     public int locationNumber; // 012 전열 345 후열 
     BattleManager battleManager;
 
-    void OnEnable()
+    void OnEnable() 
     {
         for (locationNumber = 0; locationNumber < 6; locationNumber++)
         {
+            if (heroes[locationNumber] == null) continue;
             if (heroes[locationNumber].GetHP() == 0)
             {
                 heroes[locationNumber] = null;
