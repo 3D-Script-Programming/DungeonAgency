1ae6ae67c24af7462f0b6978e282de163163698a
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-09-16 14:21:36 +0900

    CharacterFactory 리팩토링

--- Assets/Scripts/CharacterFactory.cs
+++ Assets/Scripts/CharacterFactory.cs
@@ -1,98 +1,106 @@
 ﻿using System;
-using UnityEngine;
 using System.Collections.Generic;
+
 public static class CharacterFactory
 {
-    public static System.Random random = new System.Random();
+    // 랜덤 숫자 생성기
+    private static readonly Random random = new Random();
+
+    // 몬스터 생성 메서드
     public static Character CreateMonster(int rank)
     {
-        return Create(true, rank);
+        return CreateCharacter(true, rank);
     }
 
+    // 영웅 생성 메서드
     public static Character CreateHero(int rank)
     {
-        return Create(false, rank);
+        return CreateCharacter(false, rank);
     }
 
+    // 랜덤 몬스터 생성 메서드
     public static Character CreateMonster()
     {
-        return Create(true, random.Next(21));
+        return CreateCharacter(true, random.Next(21));
     }
 
+    // 랜덤 영웅 생성 메서드
     public static Character CreateHero()
     {
-        return Create(false, random.Next(21));
+        return CreateCharacter(false, random.Next(21));
     }
 
+    // 몬스터 리스트 생성 메서드
     public static List<Character> CreateMonsterList(params int[] ranks)
     {
-        return CreateList(true, ranks);
+        return CreateCharacterList(true, ranks);
     }
 
+    // 영웅 리스트 생성 메서드
     public static List<Character> CreateHeroList(params int[] ranks)
     {
-        return CreateList(false, ranks);
+        return CreateCharacterList(false, ranks);
     }
 
+    // 몬스터 리스트 생성 메서드 (개수 지정)
     public static List<Character> CreateMonsterList(int count)
     {
-        return CreateList(true, count);
+        return CreateCharacterList(true, count);
     }
 
+    // 영웅 리스트 생성 메서드 (개수 지정)
     public static List<Character> CreateHeroList(int count)
     {
-        return CreateList(false, count);
+        return CreateCharacterList(false, count);
     }
 
-    private static Character Create(bool isMonster, int rank)
+    // 캐릭터 생성 메서드
+    private static Character CreateCharacter(bool isMonster, int rank)
     {
+        // 능력치 계산
         int statusSum = (int)Math.Pow(1.2, rank) * 50;
         int defaultStatus = statusSum * 2 / 5;
-
         statusSum -= defaultStatus;
-
         int str = random.Next(statusSum) + 1;
         int bal = random.Next(statusSum - str) + 1;
         int vtp = statusSum - bal;
 
+        // 잠재력 계산
         int potential = 10 - (int)Math.Log(random.Next(2048) + 1, 2);
 
+        // 자연 속성 설정
         Nature nature = (Nature)Enum.ToObject(typeof(Nature), random.Next(Enum.GetValues(typeof(Nature)).Length));
 
-        string prefabPath;
-        if (isMonster)
-        {
-            prefabPath = GetPrefabPath(isMonster, random.Next(Enum.GetValues(typeof(Monster)).Length));
-        }
-        else
-        {
-            prefabPath = GetPrefabPath(isMonster, random.Next(Enum.GetValues(typeof(Hero)).Length));
-        }
+        // 프리팹 경로 설정
+        string prefabPath = GetPrefabPath(isMonster, random.Next(Enum.GetValues(typeof(Monster)).Length));
+
+        // 캐릭터 생성 및 반환
         return new Character(prefabPath, str, bal, vtp, potential, nature);
     }
 
-    private static List<Character> CreateList(bool isMonster, params int[] ranks)
+    // 캐릭터 리스트 생성 메서드 (여러 랭크에 대한 리스트 생성)
+    private static List<Character> CreateCharacterList(bool isMonster, params int[] ranks)
     {
         List<Character> characters = new List<Character>();
-        foreach (int r in ranks)
+        foreach (int rank in ranks)
         {
-            characters.Add(Create(isMonster, r));
+            characters.Add(CreateCharacter(isMonster, rank));
         }
-
         return characters;
     }
 
-    private static List<Character> CreateList(bool isMonster, int count)
+    // 캐릭터 리스트 생성 메서드 (개수 지정)
+    private static List<Character> CreateCharacterList(bool isMonster, int count)
     {
         List<Character> characters = new List<Character>();
         for (int i = 0; i < count; i++)
         {
-            characters.Add(Create(isMonster, random.Next(21)));
+            characters.Add(CreateCharacter(isMonster, random.Next(21)));
         }
-
         return characters;
     }
 
+    // 프리팹 경로 생성 메서드
     private static string GetPrefabPath(bool isMonster, int prefabCode)
     {
         if (isMonster)
