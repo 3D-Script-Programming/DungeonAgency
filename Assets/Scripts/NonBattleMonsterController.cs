32bf9d351a74ae0640f207b2fc54579703cad4e6
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-09-22 17:43:07 +0900

    NonBattleMonsterController 리팩토링

--- Assets/Scripts/NonBattleMonsterController.cs
+++ Assets/Scripts/NonBattleMonsterController.cs
@@ -1,22 +1,36 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class NonBattleMonsterController : MonoBehaviour
 {
-    public GameObject slider;
-    public AnimatorController animatorController;
+    // 캐릭터 체력 바 객체
+    [SerializeField] private GameObject slider;
 
-    void OnEnable()
+    // 애니메이터 객체
+    private Animator animator;
+
+    private void Awake()
     {
-        slider.SetActive(false);
-        animatorController = new AnimatorController(GetComponent<Animator>());
-        animatorController.Victory();
+        // animator를 가져옴
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        DeactivateSlider();
+        PlayVictoryAnimation();
+    }
+
+    // 슬라이더 비활성화 함수
+    private void DeactivateSlider()
+    {
+        // 슬라이더를 비활성화
+        slider.SetActive(false);
+    }
+
+    // 등장 애니메이션 재생 함수
+    private void PlayVictoryAnimation()
+    {
+        // 'Victory' 트리거를 사용하여 등장 애니메이션 재생
+        animator.SetTrigger("Victory");
     }
 }
