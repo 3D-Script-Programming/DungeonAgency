6548b216aa1c20a0fb50fa597b14b2f104f2fa38
agy_97 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-10-05 15:42:33 +0900

    #46 - ManageListItemController 스크립트 주석 추가

--- Assets/Scripts/ManageListItemController.cs
+++ Assets/Scripts/ManageListItemController.cs
@@ -7,6 +7,7 @@ using TMPro;
 
 public class ManageListItemController : MonoBehaviour
 {
+    // UI 요소들
     public TextMeshProUGUI nameText, levelText, strText, balText, vtpText, cpText;
     public Button listItem;
     public GameObject checkOnImage;
@@ -14,12 +15,13 @@ public class ManageListItemController : MonoBehaviour
     public GameObject natureHolder;
     public GameObject natureIcon;
 
+    // 다른 클래스 참조
     private DungeonManager dungeonManager;
     public Character Monster { get; private set; }
 
     private void Start()
     {
-        ApplyEvents();
+        ApplyEvents(); // 이벤트 적용
     }
 
     private void Update()
@@ -28,20 +30,22 @@ public class ManageListItemController : MonoBehaviour
         {
             // 몬스터가 룸에 배치되어 있지 않으면
             if (Monster.CurrentRoomNumber == -1)
-                DeactivateMonsterCheck();
+                DeactivateMonsterCheck(); // 몬스터 체크 비활성화
             // 몬스터가 룸에 배치되어 있다면
             else
-                ActivateMonsterCheck();
+                ActivateMonsterCheck(); // 몬스터 체크 활성화
         }
     }
 
     void ApplyEvents()
     {
-        listItem.onClick.AddListener(OnClickListItem);
+        listItem.onClick.AddListener(OnClickListItem); // 리스트 아이템 클릭 이벤트 추가
     }
 
+    // 몬스터 정보 설정
     public void SetText(Character monster)
     {
+        // 몬스터의 속성에 따라 UI 업데이트
         if (monster.Nature == Nature.FIRE)
         {
             natureIcon.GetComponent<Image>().sprite = Resources.Load("UI/fire", typeof(Sprite)) as Sprite;
@@ -58,32 +62,36 @@ public class ManageListItemController : MonoBehaviour
             natureHolder.GetComponent<Image>().sprite = Resources.Load("UI/slider_skill_frame_green", typeof(Sprite)) as Sprite;
         }
 
+        // 몬스터 정보 텍스트 업데이트
         nameText.text = monster.Name.ToString();
         levelText.text = monster.Level.ToString();
         strText.text = monster.Strength.ToString();
         balText.text = monster.Balance.ToString();
         vtpText.text = monster.Vitality.ToString();
         cpText.text = monster.GetCP().ToString();
-        this.Monster = monster;
+        this.Monster = monster; // 현재 몬스터 설정
     }
 
+    // 리스트 아이템 클릭 시 호출
     public void OnClickListItem()
     {
-        dungeonManager.UIManager.OnClickListItem(Monster);
+        dungeonManager.UIManager.OnClickListItem(Monster); // 던전 매니저를 통해 UI 매니저 호출
     }
 
-    // MarketManager 스크립트 참조 설정
+    // 던전 매니저 설정
     public void SetDungeonManager(DungeonManager dungeonManager)
     {
         this.dungeonManager = dungeonManager;
     }
 
+    // 몬스터 체크 활성화
     public void ActivateMonsterCheck()
     {
         checkOnImage.SetActive(true);
         checkOffImage.SetActive(false);
     }
 
+    // 몬스터 체크 비활성화
     public void DeactivateMonsterCheck()
     {
         checkOnImage.SetActive(false);
